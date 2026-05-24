f6a6e58512d0dc948b61f3076d768c6a789446c1
Deseteral <deseteral@gmail.com>
2026-05-24 15:20:06 +0200

    Initial project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.strict,
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+    },
+  },
+);
+
